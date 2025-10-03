b6af5822219e63ba10add6fd2a9ebc51e77cedf7
Ido Lanuel <idolanuel@serenityplus.com>
2025-10-03 14:52:23 +0700

    Remove CSP headers and dangerous script that might be causing CSP issues

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -44,23 +44,6 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              if ('serviceWorker' in navigator) {
-                window.addEventListener('load', function() {
-                  navigator.serviceWorker.register('/sw.js')
-                    .then(function(registration) {
-                      console.log('SW registered: ', registration);
-                    })
-                    .catch(function(registrationError) {
-                      console.log('SW registration failed: ', registrationError);
-                    });
-                });
-              }
-            `,
-          }}
-        />
       </body>
     </html>
   );
